3859c36f529b9687b94eb735de8bd56cd8443b43
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-01-27 08:44:11 +0800

    add user relations to include sessions, cart, and orders in the database schema

--- src/server/db/relations.ts
+++ src/server/db/relations.ts
@@ -1,9 +1,15 @@
 import { relations } from "drizzle-orm/relations";
-import { sessions, users } from "./schema";
+import { orders, sessions, userCart, users } from "./schema";
 
 export const sessionRelations = relations(sessions, ({ one }) => ({
   users: one(users, {
     fields: [sessions.userId],
     references: [users.id],
   }),
+}));
+
+export const userRelations = relations(users, ({ many }) => ({
+  sessions: many(sessions),
+  cart: many(userCart),
+  orders: many(orders),
 }));
