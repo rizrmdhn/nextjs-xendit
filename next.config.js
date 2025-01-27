2d957c0b4a5d4dfae3c31be2ab26ede558639cb2
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-01-27 18:24:21 +0800

    update Next.js configuration and downgrade React dependencies; remove experimental settings and align versioning for React and React DOM

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {
-  experimental: {
-    reactCompiler: true,
-  },
-};
+const config = {};
 
 export default config;
