15c8e69e338545d4ba8d934a3577eba8c5fc3bbf
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-02-14 15:38:14 +0800

    Revert "fix: update order detail calculations to include currency rate for accurate pricing"
    This reverts commit 6d767d82a547adbc13c7407dac7fd4f6a3c5dc66.

--- src/app/(core)/account/orders/[id]/page.tsx
+++ src/app/(core)/account/orders/[id]/page.tsx
@@ -25,7 +25,6 @@ export default function OrderDetail() {
   });
 
   const selectedCurrency = useCurrencyStore((state) => state.currency);
-  const currencyRate = useCurrencyStore((state) => state.rate);
 
   const getStatusColor = (status: InvoiceStatus) => {
     switch (status) {
@@ -144,7 +143,7 @@ export default function OrderDetail() {
                       {new Intl.NumberFormat("en-US", {
                         style: "currency",
                         currency: selectedCurrency,
-                      }).format(item.price * item.quantity * currencyRate)}
+                      }).format(item.price * item.quantity)}
                     </li>
                   ))}
                 </ul>
@@ -159,7 +158,7 @@ export default function OrderDetail() {
                     (invoice.orderItems ?? []).reduce(
                       (acc, item) => acc + item.price * item.quantity,
                       0,
-                    ) * currencyRate,
+                    ),
                   )}
                 </p>
               </div>
